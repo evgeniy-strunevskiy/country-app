b7b017838e0b56272012e7006ffec3abfa31d516
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2025-10-27 10:52:46 +0300

    Set CountryListComponent as default route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CountryListComponent } from '@app/components/country-list/country-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CountryListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CountryListComponent } from '@app/components/country-list/country-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: CountryListComponent }];
